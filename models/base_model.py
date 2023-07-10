dcb79da4513dda96e90abdd1fed1187a04bbf3b2
Bigizic <olalekanisaac75@gmail.com>
2023-07-10 11:41:41 +0000

    Add models

--- models/base_model.py
+++ models/base_model.py
@@ -20,19 +20,29 @@ class BaseModel:
     """
 
     def __init__(self):
+        """Constructor
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
+        """Returns dict format, class name and unique id
+        """
         dct = str(self.__dict__)
         cla = self.__class__.__name__
         return ("[{}] ({}) {}" .format(cla, self.id, dct))
 
     def save(self):
+        """Update updated_at public instance attribute
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns self.__dict__, sets object class to class name,
+            converts created_at and updated_at to  string obj in
+            ISO format
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created-at'] = self.created_at.isoformat()
