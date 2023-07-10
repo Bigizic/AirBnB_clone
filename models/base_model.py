be170dc3b31394c9202b94949f6fcc40091c7805
your name <you@example.com>
2023-07-10 11:16:28 -0700

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -19,12 +19,20 @@ class BaseModel:
     """Main Class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if (key == 'created_at' or key == 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns dict format, class name and unique id
