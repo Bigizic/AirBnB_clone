4b417474d0dc21452f204e731b51306565b91746
Bigizic <olalekanisaac75@gmail.com>
2023-07-10 11:34:33 +0000

    Add models

--- models/base_model.py
+++ models/base_model.py
@@ -13,22 +13,28 @@ Return:
     void
 """
 
+import uuid
+from datetime import datetime
 class BaseModel:
     """Main Class
     """
 
     def __init__(self):
-        self.id = uuid
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        return("[{}] ({}) {{}}"
-                .format(self.__class__.__name__ , self.id, self.__dict__))
+        dct = str(self.__dict__)
+        cla = self.__class__.__name__
+        return ("[{}] ({}) {}" .format(cla, self.id, dct))
 
     def save(self):
-        self.updated_at = current_date_time
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        return
-        
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created-at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
