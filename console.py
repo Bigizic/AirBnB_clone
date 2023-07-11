f1119b507a6f56126b8b96de5823dc1c2be642f1
your name <you@example.com>
2023-07-11 10:13:29 -0700

    Task 6

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""contains the entry point of
+the command interpreter"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Representation of HBNBCommand console
+    Attributes:
+        prompt (string): the command prompt.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit is command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF is command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """shouldn’t execute anything"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+    """def default(self, arg):
+        dict_of_commands = {
+                "EOF": self.do_EOF,
+                "quit": self.do_quit
+                }
+        search = re."""
