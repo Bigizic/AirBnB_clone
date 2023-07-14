ae64c926cd86e1ab3b6b676dfd3b74cdacc6b838
Bigizic <olalekanisaac75@gmail.com>
2023-07-14 20:16:09 +0000

    Add tests

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -9,12 +9,25 @@ This module contains test for:
         json file path
     4. if reload succesffuly reloads the objects from the json file and
         if it reloads it in the correct format
+    5. if the new() succesfully creates objects for other classes, like:
+        User
+        City
+        Amenity
+        Place
+        Reviews
+        State
 """
 import unittest
 
 
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.user import User
 import json
 
 
@@ -51,9 +64,9 @@ class Test_file_storage_foundations(unittest.TestCase):
              # returns truw as it's not equal to empty object
             self.assertNotEqual(data, "")
 
+
     # test if the reload() sucessfully reloads from the file path and
-        # sets the __objects to the data it was able to read from the
-        # file
+    # sets the __objects to the data it was able to read from the file
     def test_reload_method(self):
         my_store = FileStorage()
         obj = BaseModel()
@@ -65,3 +78,68 @@ class Test_file_storage_foundations(unittest.TestCase):
         self.assertEqual(
                 my_store._FileStorage__objects[key].__class__.__name__,\
                         "BaseModel")
+
+    # test when new() is called upon User() class, if it outputs correct
+    # format
+    def test_new_user_method(self):
+        my_store = FileStorage()
+        obj = User()
+        my_store.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertEqual(key in my_store._FileStorage__objects, True)
+        self.assertEqual(my_store._FileStorage__objects[key], obj)
+
+
+    # test when new() is called upon State() class, if it outputs correct
+    # format
+    def test_new_state_method(self):
+        my_store = FileStorage()
+        obj = State()
+        my_store.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertEqual(key in my_store._FileStorage__objects, True)
+        self.assertEqual(my_store._FileStorage__objects[key], obj)
+
+
+    # test when new() is called upon City() class, if it outputs correct
+    # format
+    def test_new_city_method(self):
+        my_store = FileStorage()
+        obj = City()
+        my_store.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertEqual(key in my_store._FileStorage__objects, True)
+        self.assertEqual(my_store._FileStorage__objects[key], obj)
+
+
+    # test when new() is called upon Amenity() class, if it outputs correct
+    # format
+    def test_new_amenity_method(self):
+        my_store = FileStorage()
+        obj = Amenity()
+        my_store.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertEqual(key in my_store._FileStorage__objects, True)
+        self.assertEqual(my_store._FileStorage__objects[key], obj)
+
+
+    # test when new() is called upon Place() class, if it outputs correct
+    # format
+    def test_new_place_method(self):
+        my_store = FileStorage()
+        obj = Place()
+        my_store.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertEqual(key in my_store._FileStorage__objects, True)
+        self.assertEqual(my_store._FileStorage__objects[key], obj)
+
+
+    # test when new() is called upon Review() class, if it outputs correct
+    # format
+    def test_new_review_method(self):
+        my_store = FileStorage()
+        obj = Review()
+        my_store.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertEqual(key in my_store._FileStorage__objects, True)
+        self.assertEqual(my_store._FileStorage__objects[key], obj)
