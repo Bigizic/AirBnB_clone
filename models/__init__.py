a5bc271bb4f1488734da606b1d17e96e83a5622c
Bigizic <olalekanisaac75@gmail.com>
2023-07-16 19:28:55 +0000

    Add models

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A module that creates a unique FileStorage instance for our application
 """
-
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
