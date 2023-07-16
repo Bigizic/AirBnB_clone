d85b47112b9ee4c9bae7dd33e6d34b0181dd75f1
Bigizic <olalekanisaac75@gmail.com>
2023-07-16 22:27:16 +0000

    Add tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -9,9 +9,12 @@ This module contains test for:
 6.Test the type of the created at is datetime.
 7.Test the type of the updated at is datetime.
 """
+
 import unittest
 from models.state import State
 from datetime import datetime
+from models.base_model import BaseModel
+import time
 
 
 class Test_State_foundation(unittest.TestCase):
@@ -43,3 +46,73 @@ class Test_State_foundation(unittest.TestCase):
         s1 = State()
         s2 = State()
         self.assertNotEqual(s1.updated_at, s2.updated_at)
+
+    def test_state_inherits_from_base_model(self):
+        s1 = State()
+        self.assertIsInstance(s1, BaseModel)
+        
+    def test_state_attributes_exist(self):
+        s1 = State()
+        self.assertTrue(hasattr(s1, 'name'))
+        
+    def test_state_attributes_are_empty_strings_by_default(self):
+        s1 = State()
+        self.assertEqual(s1.name, "")
+        
+    def test_state_attributes_can_be_assigned(self):
+        s1 = State()
+        s1.name = "Canada"
+        self.assertEqual(s1.name, "Canada")
+        
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(State().updated_at))
+        
+    def test_name_is_public_str(self):
+        self.assertEqual(str, type(State.name))
+        
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(State().id))
+
+
+
+class TestState_save_method(unittest.TestCase):
+    """Unittests for testing save() for the class State
+    """
+
+    def test_state_updates_updated_at_attribute(self):
+        st = State()
+        initial_updated_at = st.updated_at
+        time.sleep(1)
+        st.save()
+        new_updated_at = st.updated_at
+        self.assertGreater(new_updated_at, initial_updated_at)
+
+
+class TestState_to_dict_method(unittest.TestCase):
+    """Unittests for testing to_dict() for the class State
+    """
+
+    def setUp(self):
+        self.st = State()
+        self.st.name = "Canada"
+        
+        def test_to_dict_returns_dictionary(self):
+            result = self.st.to_dict()
+            self.assertIsInstance(result, dict)
+            
+        def test_to_dict_contains_expected_attributes(self):
+            result = self.st.to_dict()
+
+            self.assertIn('id', result)
+            self.assertIn('created_at', result)
+            self.assertIn('updated_at', result)
+            self.assertIn('name', result)
+            
+        def test_to_dict_attribute_values_are_correct(self):
+            result = self.st.to_dict()
+            
+            self.assertEqual(result['name'], "Canada")
+
+
+if __name__ == '__main__':
+    unittest.main()
